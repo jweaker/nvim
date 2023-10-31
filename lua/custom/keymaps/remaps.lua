09d43797a01db550d087ed7502096ad58988d7cd
jweaker <jweaker.t@gmail.com>
2023-10-31 22:07:21 +0300

    better keybinds

--- lua/custom/keymaps/remaps.lua
+++ lua/custom/keymaps/remaps.lua
@@ -37,3 +37,34 @@ vim.keymap.set('o', '<M-x>', '"_x')
 
 vim.keymap.set('i', '<C-BS>', '<C-W>')
 vim.keymap.set('t', '<C-BS>', '<C-W>')
+
+
+function _G.set_terminal_keymaps()
+	local opts = { buffer = 0 }
+	vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+	vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+	vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+	vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+	vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+	vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+
+	vim.keymap.set('n', '<C-M-l>', [[<Cmd>wincmd ><CR>]])
+	vim.keymap.set('n', '<C-M-h>', [[<Cmd>wincmd <<CR>]])
+	vim.keymap.set('n', '<C-M-k>', [[<Cmd>wincmd -<CR>]])
+	vim.keymap.set('n', '<C-M-j>', [[<Cmd>wincmd +<CR>]])
+
+	vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
+
+vim.keymap.set('n', '<C-h>', [[<Cmd>wincmd h<CR>]])
+vim.keymap.set('n', '<C-j>', [[<Cmd>wincmd j<CR>]])
+vim.keymap.set('n', '<C-k>', [[<Cmd>wincmd k<CR>]])
+vim.keymap.set('n', '<C-l>', [[<Cmd>wincmd l<CR>]])
+
+vim.keymap.set('n', '<C-M-l>', [[<Cmd>wincmd ><CR>]])
+vim.keymap.set('n', '<C-M-h>', [[<Cmd>wincmd <<CR>]])
+vim.keymap.set('n', '<C-M-k>', [[<Cmd>wincmd -<CR>]])
+vim.keymap.set('n', '<C-M-j>', [[<Cmd>wincmd +<CR>]])
