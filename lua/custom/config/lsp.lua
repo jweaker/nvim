892b8deed35ef8e74e87b7dcf16058b1be16137c
jweaker <jweaker.t@gmail.com>
2023-11-08 11:25:56 +0300

    lsp

--- lua/custom/config/lsp.lua
+++ lua/custom/config/lsp.lua
@@ -54,13 +54,6 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
-
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
@@ -79,6 +72,7 @@ local servers = {
   emmet_ls = {},
   clangd = {},
   gopls = {},
+  astro = {},
 }
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
