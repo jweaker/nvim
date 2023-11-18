e47ce9299fcd514f814ac4f25d5289da1bfcc573
jweaker <jweaker.t@gmail.com>
2023-11-18 16:01:41 +0300

    @ lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -3,7 +3,16 @@ return {
   'nvim-lualine/lualine.nvim',
   -- See `:help lualine.txt`
   config = function()
-    require('lualine').setup {
+    local function show_macro_recording()
+      local recording_register = vim.fn.reg_recording()
+      if recording_register == '' then
+        return ''
+      else
+        return '@' .. recording_register
+      end
+    end
+    local lualine = require 'lualine'
+    lualine.setup {
       options = {
         icons_enabled = true,
         theme = 'auto',
@@ -23,7 +32,7 @@ return {
         },
       },
       sections = {
-        lualine_a = { 'mode' },
+        lualine_a = { 'mode', show_macro_recording },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = { 'filename' },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
@@ -43,5 +52,31 @@ return {
       inactive_winbar = {},
       extensions = { 'toggleterm', 'nvim-tree', 'fugitive', 'fzf', 'trouble', 'nvim-dap-ui', 'mason' },
     }
+    vim.api.nvim_create_autocmd('RecordingEnter', {
+      callback = function()
+        lualine.refresh {
+          place = { 'statusline' },
+        }
+      end,
+    })
+
+    vim.api.nvim_create_autocmd('RecordingLeave', {
+      callback = function()
+        -- `vim.fn.reg_recording`. If we tell lualine to refresh right now it actually will
+        -- still show a recording occuring because `vim.fn.reg_recording` hasn't emptied yet.
+        -- So what we need to do is wait a tiny amount of time (in this instance 50 ms) to
+        -- ensure `vim.fn.reg_recording` is purged before asking lualine to refresh.
+        local timer = vim.loop.new_timer()
+        timer:start(
+          50,
+          0,
+          vim.schedule_wrap(function()
+            lualine.refresh {
+              place = { 'statusline' },
+            }
+          end)
+        )
+      end,
+    })
   end,
 }
