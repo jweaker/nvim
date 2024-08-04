f37c9665ffea67c2af1baf5f4b166391d0a09919
jweaker <jweaker.t@gmail.com>
2024-08-04 16:41:17 +0300

    trouble

--- lua/custom/keymaps/keybinds.lua
+++ lua/custom/keymaps/keybinds.lua
@@ -7,11 +7,12 @@ end, { desc = 'UndoTree' })
 
 vim.keymap.set('n', '<leader>h', "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", { desc = 'Harpoon Marks' })
 vim.keymap.set('n', '<leader>m', "<cmd>lua require('harpoon.mark').add_file()<cr>", { desc = 'Mark file with harpoon' })
-vim.keymap.set('n', '<leader>lt', '<cmd>Trouble document_diagnostics<CR>', { desc = 'Trouble document' })
-vim.keymap.set('n', '<leader>lT', '<cmd>Trouble workspace_diagnostics<CR>', { desc = 'Trouble workspace' })
+vim.keymap.set('n', '<leader>lt', '<cmd>Trouble diagnostics<CR>', { desc = 'Trouble document' })
 
-vim.keymap.set('n', '<leader>pc', "<cmd>lua require('persistence').load()<cr>", { desc = 'Restore last session for current dir' })
-vim.keymap.set('n', '<leader>pl', "<cmd>lua require('persistence').load({ last = true })<cr>", { desc = 'Restore last session' })
+vim.keymap.set('n', '<leader>pc', "<cmd>lua require('persistence').load()<cr>",
+  { desc = 'Restore last session for current dir' })
+vim.keymap.set('n', '<leader>pl', "<cmd>lua require('persistence').load({ last = true })<cr>",
+  { desc = 'Restore last session' })
 vim.keymap.set('n', '<leader>pg', "<cmd>lua require('persistence').stop()<cr>", { desc = 'Quit without saving session' })
 
 vim.keymap.set('n', '<leader>ss', "<cmd>lua require('spectre').toggle()<CR>", { desc = 'Spectre' })
