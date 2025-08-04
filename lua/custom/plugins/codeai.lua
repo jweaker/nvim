f758ff37f0dacccc7b2bbc593c84eb1bae3cbae4
jweaker <jweaker.t@gmail.com>
2025-08-04 17:02:14 +0300

    ai

---
+++ lua/custom/plugins/codeai.lua
@@ -0,0 +1,34 @@
+return {
+  'olimorris/codecompanion.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+  },
+  config = function()
+    require('codecompanion').setup {
+      strategies = {
+        chat = {
+          adapter = 'copilot',
+        },
+        inline = {
+          adapter = 'copilot',
+        },
+        cmd = {
+          adapter = 'copliot',
+        },
+      },
+      display = {
+        chat = {
+          window = {
+            position = 'right',
+            width = 0.35,
+          },
+        },
+      },
+    }
+    vim.keymap.set({ 'n', 'v' }, '<C-a>', '<cmd>CodeCompanionActions<cr>', { noremap = true, silent = true })
+    vim.keymap.set({ 'n', 'v' }, '<LocalLeader>a', '<cmd>CodeCompanionChat Toggle<cr>', { noremap = true, silent = true })
+    vim.keymap.set('v', 'ga', '<cmd>CodeCompanionChat Add<cr>', { noremap = true, silent = true })
+    vim.cmd [[cab cc CodeCompanion]]
+  end,
+}
