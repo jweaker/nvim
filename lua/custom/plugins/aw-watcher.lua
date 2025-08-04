e6615b3e9d63570b6b09adc6f729af6ebdea3732
jweaker <jweaker.t@gmail.com>
2025-08-04 22:40:58 +0300

    activity watch

---
+++ lua/custom/plugins/aw-watcher.lua
@@ -0,0 +1,11 @@
+return {
+  'lowitea/aw-watcher.nvim',
+  opts = { -- required, but can be empty table: {}
+    -- add any options here
+    -- for example:
+    aw_server = {
+      host = '127.0.0.1',
+      port = 5600,
+    },
+  },
+}
