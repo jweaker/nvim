ea0b05f7827de77db7bb6aa0986fbe45b3918d61
jweaker <jweaker.t@gmail.com>
2025-11-16 17:46:21 +0300

    vim

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,30 +1,27 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = 'InsertEnter',
   config = function()
-require('copilot').setup({
-  panel = {
-    enabled = false,
-  },
-  suggestion = {
-    enabled = true,
-    auto_trigger = true,
-    hide_during_completion = true,
-    debounce = 50,
-    trigger_on_accept = true,
-    keymap = {
-      accept = "<C-f>",
-      accept_word = false,
-      accept_line = false,
-      next = "<C-l>",
-      prev = "<C-h>",
-      dismiss = "<C-D>",
-    },
-  },
-})
-
-
+    require('copilot').setup {
+      panel = {
+        enabled = false,
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        hide_during_completion = true,
+        debounce = 50,
+        trigger_on_accept = true,
+        keymap = {
+          accept = '<C-f>',
+          accept_word = false,
+          accept_line = false,
+          next = '<C-l>',
+          prev = '<C-h>',
+          dismiss = '<C-D>',
+        },
+      },
+    }
   end,
-
 }
--- lua/custom/plugins/texpresso.lua
+++
@@ -1,3 +0,0 @@
-return {
-  'let-def/texpresso.vim',
-}
